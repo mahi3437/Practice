75c87ae31fcbbbf92812bf906b09003731977d52
Naveen29-bigboxer <61657276+Naveen29-bigboxer@users.noreply.github.com>
2023-12-19 18:01:14 +0530

    Add files via upload

---
+++ BaseTest.java
@@ -0,0 +1,39 @@
+package base;
+import driver.AppFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+
+
+public class BaseTest {
+    @BeforeMethod
+    public void launchApp() throws MalformedURLException {
+        System.out.println("before method");
+        AppFactory.launchApp();
+        //base.AppiumServer.start();
+    }
+
+    @AfterMethod
+    public void closeApp(ITestResult result) throws IOException {
+        if(result.getStatus() == ITestResult.FAILURE){
+            Util.getScreenshot(result.getTestName());
+        }
+        //AppDriver.getCurrentDriver().quit();
+        //base.AppiumServer.stop();
+    }
+
+    @BeforeSuite
+    public void serverStart(){
+        System.out.println("before suite");
+        base.AppiumServer.start();
+    }
+
+    @AfterSuite
+    public void serverStop(){
+        base.AppiumServer.stop();
+    }
+}
